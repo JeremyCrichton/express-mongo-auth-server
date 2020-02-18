68c0fd572f0acf996a5b3958ad53fb2d3eb951ef
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-02-19 06:43:44 +0800

    add jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    // const newUser = await User.create(req.body); // WRONG version - any user could sign up as admin
+    const { name, email, password, passwordConfirm } = req.body;
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser
       }
@@ -16,4 +29,10 @@ exports.signup = async (req, res, next) => {
       message: 'Invalid data sent'
     });
   }
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Do stuff
 };
