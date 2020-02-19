8e781aab95492935e6165a3466ca3b3629ae2fb9
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-02-19 13:12:14 +0800

    add catchAsync util fn

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,35 +1,35 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.signup = async (req, res, next) => {
-  try {
-    // const newUser = await User.create(req.body); // WRONG version - any user could sign up as admin
-    const { name, email, password, passwordConfirm } = req.body;
-    const newUser = await User.create({
-      name,
-      email,
-      password,
-      passwordConfirm
-    });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN
-    });
+exports.signup = catchAsync(async (req, res, next) => {
+  // try {
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
-    res.status(201).json({
-      status: 'success',
-      token,
-      data: {
-        user: newUser
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data sent'
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser
+    }
+  });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(400).json({
+  //     status: 'fail',
+  //     message: 'Invalid data sent'
+  //   });
+  // }
+});
 
 exports.login = (req, res, next) => {
   const { email, password } = req.body;
